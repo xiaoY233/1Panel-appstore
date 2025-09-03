a4e1a728bec29e53d3f0a3d7bde909c4e99ee373
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:04:30 +0800

    Update App paopao-ce

--- apps/paopao-ce/0.5.2/scripts/init.sh
+++ apps/paopao-ce/0.5.2/scripts/init.sh
@@ -3,14 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
-  # setup-2 copy config file
   mkdir -p "$PAOPAO_CE_ROOT_PATH"
   mkdir -p "$PAOPAO_CE_ROOT_PATH/config"
   mkdir -p "$PAOPAO_CE_ROOT_PATH/scripts"
