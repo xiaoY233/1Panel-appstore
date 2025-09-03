b07a5267b134a8f54bc0ee44c550566bce2b005a
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:04:51 +0800

    Update App dify

--- apps/dify/1.8.0/scripts/init.sh
+++ apps/dify/1.8.0/scripts/init.sh
@@ -3,20 +3,10 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-  echo "APP_ENV_FILE=${CURRENT_DIR}/envs/dify.env" >> .env
-
-  # setup-2 update dir permissions
   mkdir -p "$DIFY_ROOT_PATH"
 
   cp -r conf/. "$DIFY_ROOT_PATH/"
 
-  # setup-3 sync environment variables
   env_source="envs/dify.env"
   if [ -f "$env_source" ]; then
     while IFS='=' read -r key value; do
--- apps/dify/1.8.0/scripts/upgrade.sh
+++ apps/dify/1.8.0/scripts/upgrade.sh
@@ -3,15 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-  echo "APP_ENV_FILE=${CURRENT_DIR}/envs/dify.env" >> .env
-
-  # setup-2 update dir permissions
   mkdir -p "$DIFY_ROOT_PATH"
 
   if [ -d "conf" ]; then
@@ -27,7 +18,6 @@ if [ -f .env ]; then
       echo "Warning: conf directory not found."
     fi
 
-  # setup-3 sync environment variables
   env_source="envs/dify.env"
   if [ -f "$env_source" ]; then
     while IFS='=' read -r key value; do
