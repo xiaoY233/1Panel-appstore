3bf520d96447922c88bbce9f019d9540ad79b597
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:00:19 +0800

    Update App weblate

--- apps/weblate/5.13.0.1/scripts/upgrade.sh
+++ apps/weblate/5.13.0.1/scripts/upgrade.sh
@@ -3,14 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
-  # setup-2 update dir permissions
   mkdir -p "$WEBLATE_ROOT_PATH"
   mkdir -p "$WEBLATE_ROOT_PATH/data"
   mkdir -p "$WEBLATE_ROOT_PATH/cache"
