e48b41efaf90a7559b254ca7f7626d05ecfbb36b
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:01:39 +0800

    Update App traccar

--- apps/traccar/6.9/scripts/init.sh
+++ apps/traccar/6.9/scripts/init.sh
@@ -3,13 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
   cp conf/traccar.xml "${TRACCAR_ROOT_PATH}/traccar.xml"
 
   echo "Check Finish."
