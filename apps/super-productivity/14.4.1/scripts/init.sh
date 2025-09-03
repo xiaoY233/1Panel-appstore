94c06d36887c946a31f204eb5e1874174bebe9cb
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:04:09 +0800

    Update App super-productivity

--- apps/super-productivity/14.4.1/scripts/init.sh
+++ apps/super-productivity/14.4.1/scripts/init.sh
@@ -3,13 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
   mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH"
   mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH/data"
   mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH/config"
