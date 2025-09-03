35475f0ee60fafaa8951d6bf44c1d0eb77d4c704
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:00:17 +0800

    Update App outline

--- apps/outline/0.86.1/scripts/upgrade.sh
+++ apps/outline/0.86.1/scripts/upgrade.sh
@@ -3,19 +3,8 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  sed -i '/^APP_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-  echo "APP_ENV_FILE=${CURRENT_DIR}/envs/outline.env" >> .env
-
-  # setup-2 remove empty values
   sed -i '/^.*=""/d' .env
 
-  # setup-3 update directory permissions
   mkdir -p "$OUTLINE_ROOT_PATH"
   mkdir -p "$OUTLINE_ROOT_PATH/data"
 
