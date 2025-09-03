85f44168160d643b32f0d37424987b71ccaa652a
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:04:11 +0800

    Update App wechatpadpro

--- apps/wechatpadpro/18.6/scripts/init.sh
+++ apps/wechatpadpro/18.6/scripts/init.sh
@@ -3,16 +3,8 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
   mkdir -p "$WECHATPADPRO_ROOT_PATH"
 
-  # WECHATPADPRO_ROOT_PATH/.env 创建空文件
   touch "$WECHATPADPRO_ROOT_PATH/.env"
 
 
