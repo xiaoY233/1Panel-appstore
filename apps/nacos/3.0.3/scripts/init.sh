270583a38d1ac462f4cfba766704d102fe542449
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:00:59 +0800

    Update App nacos

--- apps/nacos/3.0.3/scripts/init.sh
+++ apps/nacos/3.0.3/scripts/init.sh
@@ -3,13 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
   if command -v mysql &> /dev/null; then
     if [[ -f ./init/mysql-schema.sql ]]; then
       mysql -u"$MYSQL_SERVICE_USER" -p"$MYSQL_SERVICE_PASSWORD" -h"$MYSQL_SERVICE_HOST" -P"$MYSQL_SERVICE_PORT" --protocol=TCP "$MYSQL_SERVICE_DB_NAME" < ./init/mysql-schema.sql
