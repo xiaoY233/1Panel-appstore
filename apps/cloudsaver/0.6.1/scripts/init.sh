a3746357bb2a510c1d5313272f1a76768f6b4a11
xiaoY233 <xiaoY233@users.noreply.github.com>
2025-09-04 06:00:52 +0800

    Update App cloudsaver

--- apps/cloudsaver/0.6.1/scripts/init.sh
+++ apps/cloudsaver/0.6.1/scripts/init.sh
@@ -3,14 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1 add default values
-  CURRENT_DIR=$(pwd)
-  sed -i '/^ENV_FILE=/d' .env
-  sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
-  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-
-  # setup-2 copy env file
   CONFIG_DIR="$CLOUDSAVER_ROOT_PATH/config"
   mkdir -p "$CONFIG_DIR"
 
